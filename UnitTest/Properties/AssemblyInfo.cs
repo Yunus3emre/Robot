b398642bd58b582c71ba83096438db67cd5ca4f3
Yunus Emre Ayan <yemreayan@hotmail.com>
2023-03-29 15:52:50 +0300

    Unit test added.

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f96a8f1c-d1de-4e13-9704-f9e1080c456f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/RobotTest.cs
@@ -0,0 +1,71 @@
+﻿using ConsoleUI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class RobotTest
+    {
+        [TestMethod]
+        public void TestMove()
+        {
+            // Arrange
+            string instructions = "LMLMLMLMM";
+            var rover = new Robot(1, 2, 'N', 5, 5);
+
+            // Act
+            rover.Move(instructions);
+
+            // Assert
+            Assert.AreEqual(2, rover.x);
+            Assert.AreEqual(3, rover.y);
+            Assert.AreEqual('N', rover.orientation);
+        }
+        [TestMethod]
+        public void TestMove2()
+        {
+            // Arrange
+            string instructions = "MMRMMRMRRM";
+            var rover = new Robot(3, 3, 'E', 5, 5);
+
+            // Act
+            rover.Move(instructions);
+
+            // Assert
+            Assert.AreEqual(5, rover.x);
+            Assert.AreEqual(1, rover.y);
+            Assert.AreEqual('E', rover.orientation);
+        }
+        [TestMethod]
+        public void TestMove3()
+        {
+            // Arrange
+            string instructions = "MMLMMMMLMMMM";
+            var rover = new Robot(3, 3, 'N', 5, 5);
+
+            // Act
+            rover.Move(instructions);
+
+            // Assert
+            Assert.AreEqual(1, rover.x);
+            Assert.AreEqual(1, rover.y);
+            Assert.AreEqual('S', rover.orientation);
+        }
+        [TestMethod]
+        public void TestMove5()
+        {
+            // Arrange
+            string instructions = "MRMLMRMLMRMMLM";
+            var rover = new Robot(1, 1, 'N', 5, 5);
+
+            // Act
+            rover.Move(instructions);
+
+            // Assert
+            Assert.AreEqual(5, rover.x);
+            Assert.AreEqual(5, rover.y);
+            Assert.AreEqual('N', rover.orientation);
+        }
+    }
+}
